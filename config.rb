7ce0f69901c4e43c2fcea92d7273732a940fc046
Charles Ellis <hamled@hamled.name>
2017-10-24 16:09:55 -0700

    Configure Middleman S3 Sync
    In order to deploy a development version of the site first setup the
    correct variables in .env.development and then run the following
    command:
    
    bundle exec middleman s3_sync -e development

--- config.rb
+++ config.rb
@@ -52,3 +52,12 @@ end
 configure :production do
   activate :dotenv, env: '.env.production'
 end
+
+# Deployment to AWS S3
+activate :s3_sync do |s3_sync|
+  s3_sync.bucket = ENV['AWS_S3_BUCKET']
+  s3_sync.region = ENV['AWS_S3_REGION']
+
+  s3_sync.aws_access_key_id = ENV['AWS_ACCESS_KEY_ID']
+  s3_sync.aws_secret_access_key = ENV['AWS_ACCESS_KEY_SECRET']
+end
