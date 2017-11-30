292079eee038407859fbf81e99fba66274daccf9
Charles Ellis <hamled@hamled.name>
2017-11-30 10:49:53 -0800

    Use Rake tasks for deployment script

--- config.rb
+++ config.rb
@@ -7,19 +7,9 @@ page '/*.xml', layout: false
 page '/*.json', layout: false
 page '/*.txt', layout: false
 
-configure :development do
-  activate :dotenv, env: '.env.development'
-end
-
-configure :production do
-  activate :dotenv, env: '.env.production'
-end
-
 # Deployment to AWS S3
 activate :s3_sync do |s3_sync|
-  s3_sync.bucket = ENV['AWS_S3_BUCKET']
-  s3_sync.region = ENV['AWS_S3_REGION']
-
-  s3_sync.aws_access_key_id = ENV['AWS_ACCESS_KEY_ID']
-  s3_sync.aws_secret_access_key = ENV['AWS_ACCESS_KEY_SECRET']
+  # This has to be set, even though we're overriding it in Rakefile
+  s3_sync.bucket = '---not-a-real-bucket---'
+  s3_sync.region = 'us-west-2'
 end
